1aff1e08c57ef8c2f8c186240e5ceb77369b11cb
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-11-12 22:19:43 +0100

    (in progress) menu + browse file

---
+++ Scripts/SliceVideo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliceVideo : MonoBehaviour {
+
+	int i = 0;
+
+	void Update() {
+		    //Debug.Log ("test");
+		    //Application.CaptureScreenshot("Assets/Traitement_video/Screenshot"+i+".png");
+			i++;
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Application.OpenURL("file:///" + Application.dataPath + "/Resources/fileName.pdf");
+		}
+	}
+}
