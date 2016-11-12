dada075a54f231d2cef997b703e16d32828c2d0c
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-11-12 14:15:18 +0100

    Mise en places des 4 videos

---
+++ Scripts/playMovie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playMovie : MonoBehaviour {
+
+	void Start () {
+		((MovieTexture)GetComponent<RawImage>().texture).Play();
+		((MovieTexture)GetComponent<RawImage> ().texture).loop = true;
+	}
+
+	void Update () {
+	
+	}
+}
